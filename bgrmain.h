958a6e669a049d35dc9ba9b3b1e2a276e9125164
r3d9477 <r3d9477@gmail.com>
2021-01-26 16:25:31 +0300

    swap signal and slots

--- bgrmain.h
+++ bgrmain.h
@@ -10,7 +10,7 @@ class BGRMain: public QObject
 
     Q_OBJECT
 
-public slots:
+signals:
 
     //...
     //...
@@ -20,7 +20,7 @@ public:
     explicit BGRMain(QObject *parent = nullptr);
     virtual ~BGRMain();
 
-signals:
+public slots:
 
     //...
     //...
